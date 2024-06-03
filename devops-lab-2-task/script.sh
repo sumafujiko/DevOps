65c1d268a32f9ab57686d141c76e6cb1b733847b
sumafujiko <sumafujiko@gmail.com>
2024-06-03 13:53:42 +0100

    Updated to delete the folders

--- devops-lab-2-task/script.sh
+++ devops-lab-2-task/script.sh
@@ -1,11 +1,4 @@
 #!bin/bash
-mkdir fol_1
-mkdir fol_2
-# Creating files for fol_1
-touch fol_1/1_1.txt
-touch fol_1/1_2.txt
-touch fol_1/1_3.txt
-# Creating files for fol_2
-touch fol_2/2_1.txt
-touch fol_2/2_2.txt
-touch fol_2/2_3.txt
+# Updated to delete the directories
+rm -rf fol_1
+rm -rf fol_2
