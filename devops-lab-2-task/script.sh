b40224df25f10559bfe9721fd41c5ad69f79b93d
sumafujiko <sumafujiko@gmail.com>
2024-06-03 13:42:50 +0100

    Created initial file for repository

---
+++ devops-lab-2-task/script.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+mkdir fol_1
+mkdir fol_2
+# Creating files for fol_1
+touch fol_1/1_1.txt
+touch fol_1/1_2.txt
+touch fol_1/1_3.txt
+# Creating files for fol_2
+touch fol_2/2_1.txt
+touch fol_2/2_2.txt
+touch fol_2/2_3.txt
