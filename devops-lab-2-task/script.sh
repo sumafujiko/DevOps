a4430576e8abc1edb5cd6e00152b666b7052fce4
sumafujiko <sumafujiko@gmail.com>
2024-06-04 10:10:00 +0100

    Fixed script by adding in deleted lines

--- devops-lab-2-task/script.sh
+++ devops-lab-2-task/script.sh
@@ -1,4 +1,14 @@
 #!bin/bash
-# Updated to delete the directories
-rm -rf fol_1
-rm -rf fol_2
+mkdir fol_1
+mkdir fol_2
+#Delete directories
+rm -r fol_1
+rm -r fol_2
+# Creating files for fol_1
+touch fol_1/1_1.txt
+touch fol_1/1_2.txt
+touch fol_1/1_3.txt
+# Creating files for fol_2
+touch fol_2/2_1.txt
+touch fol_2/2_2.txt
+touch fol_2/2_3.txt
